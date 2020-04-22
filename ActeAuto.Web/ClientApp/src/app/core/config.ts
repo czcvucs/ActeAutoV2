464a0893bdedfdd3b3d2527a573ac1de7293258f
David Ardeiu <david.ardeiu@nagarro.com>
2020-04-22 16:17:28 +0300

    Add Redirect URI

--- ActeAuto.Web/ClientApp/src/app/core/config.ts
+++ ActeAuto.Web/ClientApp/src/app/core/config.ts
@@ -1,7 +1,8 @@
 export const authConfig = {
     tenant: '95898435-bd2b-4173-991f-2f694f01ffa1',
     clientId: '02dbf031-951d-4b6a-9f69-641204b6e31a',
-    redirectUri: window.location.origin + '/login-callback',
+    redirectUri: 'https://acteauto.azurewebsites.net/login-callback',
+    //redirectUri: window.location.origin + '/login-callback',
     endpoints: {
         'api application url': 'api application client id', // this is for feteching the access token  
     },
